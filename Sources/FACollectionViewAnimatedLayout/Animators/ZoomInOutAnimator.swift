027c64ee4ec2b24074e85d56e09e8123caabf837
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-03-08 21:54:56 +0300

    Alpha offset for the ZoomInOut animator

--- Sources/FACollectionViewAnimatedLayout/Animators/ZoomInOutAnimator.swift
+++ Sources/FACollectionViewAnimatedLayout/Animators/ZoomInOutAnimator.swift
@@ -13,12 +13,22 @@ public struct ZoomInOutAnimator: LayoutAnimator {
     /// 1 means the cell will disappear at min. 0.2 by default.
     public var scaleRate: CGFloat
     
-    public init(scaleRate: CGFloat = 0.2) {
+    /// Alpha value offset for the cells outside the center
+    public var alphaOffset: CGFloat
+    
+    private var finalAlpha: CGFloat
+    
+    public init(scaleRate: CGFloat = 0.2, alphaOffset: CGFloat = 0.5) {
         self.scaleRate = scaleRate
+        self.alphaOffset = alphaOffset
+        self.finalAlpha = 1 - self.alphaOffset
     }
     
     public func animate(collectionView: UICollectionView, attributes: AnimatedLayoutAttributes) {
         let position = attributes.middleOffset
+        
+        let alpha: CGFloat = 1 - min(abs(position), finalAlpha)
+        attributes.alpha = alpha
         
         if position <= 0 && position > -1 {
             let scaleFactor = scaleRate * position + 1.0
